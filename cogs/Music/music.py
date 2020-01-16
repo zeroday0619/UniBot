d5eccc4352e69f7362ad9e2c46fd919e8c9df5b0
zeroday0619 <zeroday0619@zeroday0619.kr>
2020-01-16 12:13:31 +0900

    Update music.py

--- cogs/Music/music.py
+++ cogs/Music/music.py
@@ -1,8 +1,3 @@
-"""
-Powered By ZERODAY BOT
-Copyright (C) 2019 All Rights Reserved Zeroday Cha
-Closed Source | Uni Bot Project 한정 Project ZERODAY 소스 공유
-"""
 import asyncio
 import discord
 import traceback
