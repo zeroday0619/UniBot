539835c521b0bb5830c49814388dd19d28893289
zeroday0619 <zeroday0619@kakao.com>
2020-02-03 08:52:27 +0900

    Twitch.py Class 사용

--- cogs/Twitch/twitch.py
+++ cogs/Twitch/twitch.py
@@ -1,45 +1,56 @@
 import requests
 import polling
 
+class TwitchStreamAlert:
+    def __init__(self, client_id, client_secret):
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+    def oauth2(self):
+        url = "https://id.twitch.tv/oauth2/token"
+        params = {
+            'client_id': f'{self.client_id}',
+            'client_secret': f'{self.client_secret}',
+            'grant_type': 'client_credentials'
+        }
+        res = requests.post(url=url, params=params)
+        return res.json()['access_token']
+
+    def TwitchApi(self, oauth, name):
+        url = "https://api.twitch.tv/helix/" + "users"
+        headers = {
+            'Authorization': f'Bearer {oauth}'
+        }
+        params = {
+            'login': f'{name}'
+        }
+        res = requests.get(url=url, headers=headers, params=params).json()['data'][0]['id']
+        return res
+
 
 def is_livestream(response):
     if response == {'data': [], 'pagination': {}}:
         return False
     else:
-        return response == response
+        return True
+class StreamAlert(TwitchStreamAlert):
+    def __init__(self, client_id, client_secret):
+        super().__init__(client_id, client_secret)
+    def main(self, name):    
+        url = "https://api.twitch.tv/helix/streams"
+        headers = {
+            'Client-ID': f'{self.client_id}'
+        }
+        json = {
+            'user_id': f'{self.TwitchApi(oauth=self.oauth2(), name=name)}'
+        }
+        app = polling.poll(
+            lambda: requests.get(url=url, headers=headers, params=json).json(),
+            check_success=is_livestream,
+            step=5,
+            poll_forever=True
+        )
+        print(app)
 
-def oauth2():
-    url = "https://id.twitch.tv/oauth2/token"
-    params = {
-        'client_id': '',
-        'client_secret': '',
-        'grant_type': 'client_credentials'
-    }
-    res = requests.post(url=url, params=params)
-    return res.json()['access_token']
-
-def TwitchApi():
-    url = "https://api.twitch.tv/helix/" + "users"
-    headers = {
-        'Authorization': f'Bearer {oauth2()}'
-    }
-    params = {
-        'login': ''
-    }
-    res = requests.get(url=url, headers=headers, params=params).json()['data'][0]['id']
-    return res
-    
-url = "https://api.twitch.tv/helix/streams"
-headers = {
-    'Client-ID': ''
-}
-json = {
-    'user_id': TwitchApi()
-}
-app = polling.poll(
-    lambda: requests.get(url=url, headers=headers, params=json).json(),
-    check_success=is_livestream,
-    step=5,
-    poll_forever=True
-)
-print(app)
+# src = StreamAlert(client_id="", client_secret="")
+# src.main(name="")
